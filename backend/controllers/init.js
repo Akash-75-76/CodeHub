ea6d88bf4cce665e02e59548245df0b6166b4d40
Akash-75-76 <deshmukhaakash752005@gmail.com>
2025-08-14 18:03:27 +0530

    Worked on init repo logic

--- backend/controllers/init.js
+++ backend/controllers/init.js
@@ -1,5 +1,19 @@
+const fs=require("fs").promises;
+const path=require("path");
 async function initRepo() {
-  console.log("Initializing repository...");
+  const repoPath=path.resolve(process.cwd(),".codehubGit");
+  const commitsPath=path.join(repoPath,"commits");
+  try {
+    await fs.mkdir(repoPath,{ recursive: true });
+    await fs.mkdir(commitsPath,{ recursive: true });
+    await fs.writeFile(
+        path.join(repoPath,"config.json"),
+        JSON.stringify({ bucket: "your-s3-bucket-name" })
+    )
+    console.log("Repository initialized successfully.");
+  } catch (error) {
+    console.log(`Error initializing repository: ${error.message}`);
+  }
 }
 
 module.exports = { initRepo };
