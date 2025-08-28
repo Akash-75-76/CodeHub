f958d2250244564a920776616b7944ec9b6ed08a
Akash-75-76 <deshmukhaakash752005@gmail.com>
2025-08-28 15:00:04 +0530

    `Added Express.js server setup and MongoDB connection in backend/index.js, updated dependencies in package-lock.json and package.json`

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,20 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const bodyparser = require("body-parser");
+const http = require("http");
+
+dotenv.config();
+
 const yargs = require("yargs");
 const { hideBin } = require("yargs/helpers");
 const { initRepo } = require("./controllers/init");
 const { addRepo } = require("./controllers/add");
-const {commitRepo}=require("./controllers/commit");
-const {pushRepo}=require("./controllers/push");
-const {pullRepo}=require("./controllers/pull");
-const {revertRepo}=require("./controllers/revert");
+const { commitRepo } = require("./controllers/commit");
+const { pushRepo } = require("./controllers/push");
+const { pullRepo } = require("./controllers/pull");
+const { revertRepo } = require("./controllers/revert");
 yargs(hideBin(process.argv))
   .command("start", "Starts a new server", {}, startServer)
   .command("init", "Initialize a new repository", {}, initRepo)
@@ -43,9 +52,27 @@ yargs(hideBin(process.argv))
       });
     },
     revertRepo
-  )
-  .argv;
+  ).argv;
 
-  function startServer() {
-    console.log("Starting server...");
-  }
+function startServer() {
+  const app = express();
+  app.use(cors());
+  app.use(bodyparser.json());
+  app.use(bodyparser.urlencoded({ extended: true }));
+  const port = process.env.PORT || 3000;
+  mongoose
+    .connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log("Connected to MongoDB");
+      const server = http.createServer(app);
+      server.listen(port, () => {
+        console.log("Server is running on port 5000");
+      });
+    })
+    .catch((error) => {
+      console.error("MongoDB connection error:", error);
+    });
+}
